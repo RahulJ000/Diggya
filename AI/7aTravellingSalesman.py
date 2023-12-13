a2ef81f89f377a9d746aa2007fa385325fee5878
RahulJ000 <153193347+RahulJ000@users.noreply.github.com>
2023-12-13 17:20:43 +0530

    Update 7aTravellingSalesman.py

--- AI/7aTravellingSalesman.py
+++ AI/7aTravellingSalesman.py
@@ -18,8 +18,6 @@ def travellingSalesmanProblem(graph, s):
         min_path = min(min_path, current_pathweight)
     return min_path
  
- 
-
 if __name__ == "__main__":
     graph = [[0, 10, 15, 20], [10, 0, 35, 25],
             [15, 35, 0, 30], [20, 25, 30, 0]]
